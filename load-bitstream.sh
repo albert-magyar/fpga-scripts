d5c04965a7e03b4754ff948ca7958c2ae0ef0b29
Albert Magyar <magyar@eecs.berkeley.edu>
2015-08-04 10:16:17 -0700

    Use base script directory in tcl script path

--- load-bitstream.sh
+++ load-bitstream.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 echo $FPGA_JTAG_LOCATOR
-vivado -mode batch -source load-bitstream.tcl -tclargs $FPGA_JTAG_LOCATOR $1
+vivado -mode batch -source $(SCRIPT_DIR)/load-bitstream.tcl -tclargs $FPGA_JTAG_LOCATOR $1
