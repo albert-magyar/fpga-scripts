6e371e8c86ce5b15f72f50acfd0f8699d59aa31a
Albert Magyar <magyar@eecs.berkeley.edu>
2015-08-04 10:32:45 -0700

    Prompt user to allow using default Vivado if none on path

--- load-bitstream.sh
+++ load-bitstream.sh
@@ -3,4 +3,15 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo $FPGA_JTAG_LOCATOR
-vivado -mode batch -source $(SCRIPT_DIR)/load-bitstream.tcl -tclargs $FPGA_JTAG_LOCATOR $1
+
+if [[ `which vivado` ]]; then
+    echo -n "Using Vivado: "
+    which vivado
+else
+    echo -n "No Vivado in path, using 2014.4. Press ENTER to continue."
+    read OKAY_RESP
+    source /ecad/tools/fpga.bashrc
+    source /ecad/tools/xilinx/2014.4/settings64.sh
+fi
+
+vivado -mode batch -source ${SCRIPT_DIR}/load-bitstream.tcl -tclargs $FPGA_JTAG_LOCATOR $1
